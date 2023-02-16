7f39d30acbcb614a05cceaf03c297241648c9ca5
Digambar S <erdigambar@gmail.com>
2023-02-16 18:21:28 +0530

    changes

---
+++ src/features/counter/book/components/Header/Header.js
@@ -0,0 +1,36 @@
+import "./Header.css"
+
+function Header (){
+    return(
+        <nav className="navbar navbar-expand-lg bg-body-tertiary">
+  <div className="container-fluid">
+    <a className="navbar-brand" href="#">BookReads</a>
+    <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+      <span className="navbar-toggler-icon"></span>
+    </button>
+    <div className="collapse navbar-collapse" id="navbarSupportedContent">
+      <ul className="navbar-nav me-auto mb-2 mb-lg-0 flex-menus">
+        <li className="nav-item">
+          <a className="nav-link active" aria-current="page" href="#">Home</a>
+        </li>
+        <li className="nav-item">
+          <a className="nav-link" href="#">Register</a>
+        </li>
+        <li className="nav-item">
+          <a className="nav-link" href="#">Login</a>
+        </li>
+        <li className="nav-item dropdown">
+          <a className="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+          </a>
+        </li>
+        <li className="nav-item">
+          <a className="nav-link disabled">Disabled</a>
+        </li>
+      </ul>
+
+    </div>
+  </div>
+</nav>
+    )
+}
+export default Header;  
